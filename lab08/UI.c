c8ad3809c258f14b394e02858ad5ef9349db5ab3
unknown <k_szeto1@u.pacific.edu>
2021-11-27 21:14:58 -0800

    UI

--- lab08/UI.c
+++ lab08/UI.c
@@ -68,7 +68,7 @@ static const uint8_t keymap[4][4] = {
 static const uint8_t * vol {
     '01', '02', '03', '04', "05", "06", "07", "08",
     "09", "10", "11", "12", "13", "14", "15", "16",
-    "17", "18", "19", "20", "21", "22", "23", "24",
+    "17", "18", "19", "2rem0", "21", "22", "23", "24",
     "25", "26", "27", "28", "29", "30", "31", "32",
 };
 */
@@ -175,7 +175,7 @@ void UIHandler( void ) {
             secondMenuFlag = false;
             setDone();
         }else{
-            enterQueueMode();
+            //enterQueueMode();
         }
       //setDone();
       break;
